2805c97af6b8c128802f2e6938840580b98c42c8
Lukas Masuch <lukas.masuch@gmail.com>
2020-12-23 00:29:36 +0100

    Improve utilities to gather and improve projects

--- src/best_of/utils.py
+++ src/best_of/utils.py
@@ -51,3 +51,19 @@ def process_description(text: str, max_lenght: int) -> str:
         text += "."
 
     return clean_whitespaces(textwrap.shorten(text, width=max_lenght, placeholder=".."))
+
+
+url_validator = re.compile(
+    r"^(?:http|ftp)s?://"  # http:// or https://
+    r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"  # domain...
+    r"localhost|"  # localhost...
+    r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or ip
+    r"(?::\d+)?"  # optional port
+    r"(?:/?|[/?]\S+)$",
+    re.IGNORECASE,
+)
+
+
+def is_valid_url(url: str) -> bool:
+    """Check is the provided URL is valid."""
+    return re.match(url_validator, url) is not None
--- src/best_of/yaml_generation.py
+++ src/best_of/yaml_generation.py
@@ -1,82 +1,194 @@
+import copy
+import logging
+import os
 import re
+import urllib.request
+from typing import List, Optional
 
 import requirements
 import yaml
 from addict import Dict
 from tqdm import tqdm
 
-from best_of.integrations import github_integration, pypi_integration
+from best_of import projects_collection, utils
+from best_of.integrations import (
+    conda_integration,
+    github_integration,
+    npm_integration,
+    pypi_integration,
+)
+
+log = logging.getLogger(__name__)
 
 
-def extract_github_projects_to_yaml(input_str: str, yaml_output_path: str) -> None:
+def extract_github_projects(
+    input: str, excluded_github_ids: Optional[List[str]] = None
+) -> list:
+
+    excluded_projects = set()
+    added_projects = set()
+
+    if excluded_github_ids:
+        for excluded_project in excluded_github_ids:
+            excluded_projects.add(utils.simplify_str(excluded_project))
+
+    input_str = input
+    if os.path.isfile(input):
+        # If input is a valid file path, read as file
+        with open(input, "r") as f:
+            input_str = f.read()
+    elif utils.is_valid_url(input):
+        # if input is a valid url, open and read from url
+        filedata = urllib.request.urlopen(input)
+        input_str = filedata.read().decode("utf-8")
+
     # extract github project urls
     projects = []
     for github_match in tqdm(
         re.findall(
-            r"(^|[^#])https:\/\/github\.com\/(.*?)(\s|$)", input_str, re.MULTILINE
+            r"(^|[^#])https:\/\/github\.com\/([a-zA-Z0-9-_.]*\/[a-zA-Z0-9-_.]*)",
+            input_str,
+            re.MULTILINE,
         )
     ):
         if len(github_match) <= 1:
             continue
 
-        github_id = github_match[1].rstrip("/").strip()
+        github_id = github_match[1].strip().rstrip("/").rstrip(".")
         if not github_id:
+            continue
+
+        if utils.simplify_str(github_id) in added_projects:
+            # project already added
+            continue
+
+        if utils.simplify_str(github_id) in excluded_projects:
+            # skip excluded projects
             continue
 
         project = Dict()
         project.github_id = github_id
         github_integration.update_via_github(project)
-        projects.append(project)
 
-    output_yaml = []
-    for project in projects:
-        project_output = {}
+        if not project.github_url or not project.name:
+            # did not fetch any data from github, do not add project
+            continue
 
-        if project.name:
-            project_output["name"] = project.name
+        if project.updated_github_id:
+            if utils.simplify_str(project.updated_github_id) in excluded_projects:
+                # project is added with updated id
+                continue
 
-        if project.github_id:
-            if not project.name:
-                project_output["name"] = project.github_id.split("/")[1]
-            project_output["github_id"] = project.github_id
+            # Apply updated github id:
+            project.github_id = project.updated_github_id
+            added_projects.add(utils.simplify_str(project.updated_github_id))
 
-        output_yaml.append(project_output)
+        project.projectrank = projects_collection.calc_projectrank(project)
 
-    with open(yaml_output_path, "w") as f:
-        yaml.dump(output_yaml, f, default_flow_style=False, sort_keys=False)
+        added_projects.add(utils.simplify_str(github_id))
+        projects.append(project.to_dict())
+
+    return projects
 
 
-def requirements_to_yaml(requirements_path: str, yaml_output_path: str) -> None:
-    requirements_projects = []
-    with open(requirements_path, "r") as fd:
-        for req in tqdm(requirements.parse(fd)):
-            project = Dict()
-            project.pypi_id = req.name
-            pypi_integration.update_via_pypi(project)
-            requirements_projects.append(project)
+def extract_pypi_projects_from_requirements(
+    requirements_input: str, excluded_pypi_ids: Optional[List[str]] = None
+) -> list:
+    # libraries.io should be configured
+    excluded_projects = set()
+    added_projects = set()
 
-    output_yaml = []
-    for project in requirements_projects:
-        project_output = {}
+    if excluded_pypi_ids:
+        for excluded_project in excluded_pypi_ids:
+            excluded_projects.add(utils.simplify_str(excluded_project))
 
-        if project.name:
-            project_output["name"] = project.name
+    input_str = requirements_input
+    if os.path.isfile(requirements_input):
+        # If input is a valid file path, read as file
+        with open(requirements_input, "r") as f:
+            input_str = f.read()
+    elif utils.is_valid_url(requirements_input):
+        # if input is a valid url, open and read from url
+        filedata = urllib.request.urlopen(requirements_input)
+        input_str = filedata.read().decode("utf-8")
 
-        if project.pypi_id:
-            if not project.name:
-                project_output["name"] = project.pypi_id
-            project_output["pypi_id"] = project.pypi_id
+    projects = []
+    for req in tqdm(requirements.parse(input_str)):
+        pypi_id = req.name.strip().lower()
 
-        if project.github_id:
-            project_output["github_id"] = project.github_id
+        if utils.simplify_str(pypi_id) in added_projects:
+            # project already added
+            continue
 
-        output_yaml.append(project_output)
+        if utils.simplify_str(pypi_id) in excluded_projects:
+            # skip excluded projects
+            continue
 
-    # define a custom representer for strings
-    # def quoted_presenter(dumper, data):
-    #    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='"')
+        project = Dict()
+        project.pypi_id = pypi_id
+        pypi_integration.update_via_pypi(project)
 
-    # yaml.add_representer(str, quoted_presenter)
+        if not project.monthly_downloads:
+            # did not fetch any data from github, do not add project
+            continue
 
-    with open(yaml_output_path, "w") as f:
-        yaml.dump(output_yaml, f, default_flow_style=False, sort_keys=False)
+        project.projectrank = projects_collection.calc_projectrank(project)
+        added_projects.add(utils.simplify_str(pypi_id))
+        projects.append(project.to_dict())
+
+    return projects
+
+
+def auto_extend_package_manager(
+    projects: list, pypi: bool = False, conda: bool = False, npm: bool = False
+) -> list:
+    updated_projects = []
+    for project in tqdm(projects):
+        project = Dict(project)
+        project_name = project.name.lower().strip().replace(" ", "-")
+        if pypi and not project.pypi_id:
+            project_cloned = copy.deepcopy(project)
+            project_cloned.pypi_id = project_name
+            pypi_integration.update_via_pypi(project_cloned)
+            if (
+                project_cloned.pypi_monthly_downloads
+                and project_cloned.pypi_monthly_downloads > 500
+            ):
+                # only use this package if monthly downloads over 500 -> to prevent errors
+                log.info(
+                    f"Detected pypi package: {project_cloned.pypi_id} for project {project_cloned.name}."
+                )
+                project = project_cloned
+
+        if conda and not project.conda_id:
+            project_cloned = copy.deepcopy(project)
+            project_cloned.conda_id = "conda-forge/" + project_name
+            conda_integration.update_via_conda(project_cloned)
+            if (
+                project_cloned.conda_total_downloads
+                and project_cloned.conda_total_downloads > 5000
+            ):
+                # only use this package if total downloads over 5000 -> to prevent errors
+                log.info(
+                    f"Detected conda package: {project_cloned.conda_id} for project {project_cloned.name}."
+                )
+                project = project_cloned
+
+        if npm and not project.npm_id:
+            project_cloned = copy.deepcopy(project)
+
+            project_cloned.npm_id = project_name
+            npm_integration.update_via_npm(project_cloned)
+            if (
+                project_cloned.npm_monthly_downloads
+                and project_cloned.npm_monthly_downloads > 500
+            ):
+                # only use this package if monthly downloads over 500 -> to prevent errors
+                log.info(
+                    f"Detected npm package: {project_cloned.npm_id} for project {project_cloned.name}."
+                )
+                project = project_cloned
+        # recalculated projectrank
+        project.projectrank = projects_collection.calc_projectrank(project)
+        updated_projects.append(project)
+    return [project.to_dict() for project in updated_projects]
