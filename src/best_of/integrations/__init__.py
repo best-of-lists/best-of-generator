56a733998fb8dc405d9e3288a2da887a40531f43
Lukas Masuch <lukas.masuch@gmail.com>
2020-12-23 00:24:06 +0100

    Add list of available package managers

---
+++ src/best_of/integrations/__init__.py
@@ -0,0 +1,3 @@
+AVAILABLE_PACKAGE_MANAGER = ["conda", "pypi", "dockerhub", "maven", "npm"]
+
+# TODO: "helm_id", "brew_id", "apt_id", "yum_id", "snap_id", "cargo_id", "maven_id", "dnf_id", "yay_id",
